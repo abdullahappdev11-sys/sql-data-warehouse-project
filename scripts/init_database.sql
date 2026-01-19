dfe901c662ef66eaeab21dafec32ede3f177bf2f
abdullahappdev11-sys <abdullahappdev11@gmail.com>
2026-01-19 01:31:49 -0800

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,20 @@
+
+use master;
+
+--Create Database
+CREATE DATABASE DataWarehouse;
+
+
+use DataWarehouse
+Go
+
+--Create Schemas
+Create SCHEMA bronze;
+GO
+
+Create SCHEMA silver;
+GO
+
+Create SCHEMA gold;
+GO
+
